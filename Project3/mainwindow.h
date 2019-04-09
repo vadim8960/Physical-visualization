f333b161eb2dc07e3d15d2db40849df6d26f4f4a
Vadim <akimovvadim779@gmail.com>
2019-04-09 19:21:05 +0300

    Added ccomments

--- Project3/mainwindow.h
+++ Project3/mainwindow.h
@@ -6,12 +6,14 @@
 
 #include <QTimer>
 
+// Состояния кнопки Freeze
 enum {
     FREEZE,
     CONTINUE,
     WAIT
 };
 
+// Объявление пространста имен для икслючения корреляции имен
 namespace Ui {
 class MainWindow;
 }
@@ -21,29 +23,29 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr); // Конструктор
+    ~MainWindow();                                  // Деструктор
 
 signals:
-    void status(unsigned st);
+    void status(unsigned st);       // Сигнал настройки состояния виджета OpenglWidget
 
 private slots:
-    void on_btn_show_clicked();
-    void on_btn_start_clicked();
-    void on_btn_stop_clicked();
-    void on_btn_freeze_clicked();
-    void start_timer();
-    void show_params();
+    void on_btn_show_clicked();     // Слот нажатия кнопки Show
+    void on_btn_start_clicked();    // Слот нажатия кнопки Start
+    void on_btn_stop_clicked();     // Слот нажатия кнопки Stop
+    void on_btn_freeze_clicked();   // Слот нажатия кнопки Freeze
+    void start_timer();             // Слот запуска таймера для начала анимации
+    void show_params();             // Слот для вывода текущих параметров системы
 
 private:
-    Ui::MainWindow *ui;
-    OpenglWidget *simulation;
-    QTimer *timer;
+    Ui::MainWindow *ui;             // Основное окно
+    OpenglWidget *simulation;       // Виджет OpenglWidget
+    QTimer *timer;                  // Таймер для обновления виджета OpenglWidget
 
-    unsigned status_button;
+    unsigned status_button;         // Текущее состояние кнопки Freeze
 
-    void show_message(QString msg = "");
-    bool check_is_empty();
+    void show_message(QString msg = "");   // Вывод ошибки
+    bool check_is_empty();                 // Проверка на корректность введенных данных
 };
 
 #endif // MAINWINDOW_H
